b5f39427c1a66add34041d3cb1f478474e8d30f3
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2020-08-29 17:11:55 +0800

    init

---
+++ docs/basic.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import Component from '../src/index';
+
+export default () => <Component />;
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+export default () => {
+  return <div>My Component</div>;
+};
